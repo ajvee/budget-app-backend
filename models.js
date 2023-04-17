a957b27b7ff69c7d6068a38bdd6a935ef80b578d
Angel <EMAIL>
2023-04-17 19:52:32 -0400

    first commit

---
+++ models.js
@@ -0,0 +1,7 @@
+module.exports = [
+    { id: 1, item_name: 'income', amount: 1000, date: '2023-04-17', from: 'employer', category: 'income' },
+    { id: 2, item_name: 'savings', amount: 500, date: '2023-04-16', from: 'bank', category: 'savings' },
+    { id: 3, item_name: 'cat food', amount: 20, date: '2023-04-16', from: 'pet store', category: 'pets' },
+    { id: 4, item_name: 'groceries', amount: 50, date: '2023-04-15', from: 'grocery store', category: 'food' }
+  ];
+  
