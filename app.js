2f003c9953520869a65f36978008212fe4972313
Angel <EMAIL>
2023-04-17 20:42:46 -0400

    single transaction

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
-const budget = require("./models/Budget");
+const transactions = require("./models/Transactions");
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Budget App")
-})
+// Get all transactions
+app.get('/transactions', (req, res) => {
+    res.json(transactions);
+  });
 
-
-
-
+// Get a single transaction by ID
+app.get('/transactions/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const transaction = transactions.find(t => t.id === id);
+    if (transaction) {
+      res.json(transaction);
+    } else {
+      res.sendStatus(404);
+    }
+  });
 
 
 module.exports = app;
